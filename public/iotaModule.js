4e5f28a145772b6b34448e4a85683221987292e1
Maying(Matt) Shi <himatt93@gmail.com>
2019-09-03 11:00:22 -0700

    Add files via upload

---
+++ public/iotaModule.js
@@ -0,0 +1,122 @@
+
+// Require the IOTA libraries
+const Iota = require('@iota/core');
+//import { composeAPI } from '@iota/core'
+const Converter = require('@iota/converter');
+// Create a new instance of the IOTA object
+// Use the `provider` field to specify which IRI node to connect to
+const iota = Iota.composeAPI({
+    //provider: 'http://node01.iotatoken.nl:14265'
+    provider: 'https://nodes.devnet.iota.org:443'
+});
+
+module.exports = {
+
+  /**
+  * send a transaction
+  * @param  {String} sender_seed address sending the transaction
+  * @param  {String} recv_address address to receive the transaction
+  * @param  {String} sending_message the message in the transaction
+  */
+  sendTranscation: function (sender_seed, recv_address, sending_message){
+    const seed = sender_seed;
+    const address = recv_address;
+    const message = Converter.asciiToTrytes(sending_message);
+
+    // Construct a TX to our new address
+    const transfers = [
+        {
+            value: 0,
+            address: address,
+            message: message,
+            tag: "SDPP"
+        }
+    ];
+
+    return iota.prepareTransfers(seed, transfers)
+        .then(trytes => {
+            return iota.sendTrytes(trytes, 3/*depth*/, 14/*minimum weight magnitude*/)
+            //min mwm is 9 for devnet, 14 for mainnet
+        })
+        .then(bundle => {
+            //console.log(`Bundle: ${JSON.stringify(bundle, null, 1)}`)
+            return bundle[0].hash;
+        })
+        .catch(err => {
+            // Catch any errors
+            console.log(err);
+        });
+    },
+
+  /**
+  * find an transcation
+  * @param  {String} addressToFetch the address the transcations belong to
+  */
+  fetchTransaction: function (addressToFetch){
+    const address = addressToFetch;
+
+    iota
+        .findTransactionObjects({ addresses: [address] })
+        .then(response => {
+            console.log(response)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+  },
+
+  /**
+  * send tokens
+  * @param  {String} sender_seed address sending the transaction
+  * @param  {String} recv_address address to receive the transaction
+  * @param  {Number} amount the amount of tokens to be sent
+  */
+  sendTokens: function (sender_seed, recv_address, amount, sending_message){
+    const seed = sender_seed;
+    const address = recv_address;
+    const message = Converter.asciiToTrytes(sending_message);
+
+    // Construct a TX to our new address
+    const transfers = [
+        {
+            value: amount,
+            address: address,
+            message: message,
+            tag: "SDPP"
+        }
+    ];
+
+    return iota.prepareTransfers(seed, transfers)
+        .then(trytes => {
+            return iota.sendTrytes(trytes, 3/*depth*/, 14/*minimum weight magnitude*/)
+            //min mwm is 9 for devnet, 14 for mainnet
+        })
+        .then(bundle => {
+            //console.log(`Bundle: ${JSON.stringify(bundle, null, 1)}`)
+            //console.log('.then:', bundle[0].hash)
+            return bundle[0].hash;
+        })
+        .catch(err => {
+            // Catch any errors
+            console.log(err);
+        });
+  },
+
+  /**
+  * check the balance of the chosen address
+  * @param  {String} check_address address to be checked
+  */
+  checkBalance: function (check_address){
+    const address = check_address;
+
+    iota
+        .getBalances([address], 100)
+        .then(({ balances }) => {
+            console.log(balances)
+        })
+        .catch(err => {
+            console.error(err)
+        })
+  },
+
+};
---
+++ seller/index.js
@@ -0,0 +1,89 @@
+//const cv = require('opencv4nodejs')
+//global.cv = require('/usr/lib/node_modules/opencv4nodejs')
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
+const path = require('path');
+const { getVideoDurationInSeconds } = require('get-video-duration')
+
+const io = require('socket.io').listen(process.env.PORT || 3000);
+const iota = require('./iotaModule');
+const fs = require('fs');
+
+const FPS = 20;
+var payment_granularity = 500;
+var cost = 2;
+var duration = 0;
+
+
+function pad(num, size) {
+    var s = num+"";
+    while (s.length < size) s = "0" + s;
+    return s;
+}
+
+getVideoDurationInSeconds('toystory.mp4').then((dur) => {
+  duration = dur;
+})
+
+ffmpeg('toystory.mp4')
+    .fps(36)
+    .on('end', function() {
+      console.log('Screenshots taken');
+    })
+    .output('frames-%04d.jpg')
+    .run()
+
+
+  // for (i = 1; i < 10; i++) {
+  //   const frame_name = '../frames/frames-' + pad(i,4) +'.jpg';
+  //   console.log('read ',frame_name);
+  // }
+
+
+io.sockets.on('connection',(socket) =>{
+  console.log('Connected to client!')
+  var imageCount = 0;
+  var isPaused = 0;
+  var index = 1;
+  setInterval(() => {
+    //console.log('count:',imageCount)
+    //console.log('paused:',isPaused)
+    if(index>duration*36){index =1;}
+    if(isPaused == 0){
+      if(imageCount % payment_granularity == 0){
+        isPaused = 1;
+        console.log('*** Paused, wait for payment ***')
+        socket.emit('DATA_INVOICE', payment_granularity);
+      }
+      // const frame = wCap.read();
+      // const image = cv.imencode('.jpg',frame).toString('base64');
+      // socket.emit('image',image);
+      //const frame_name = '../frames/frames-' + pad(index,4) +'.jpg';
+      //console.log('read ',frame_name);
+
+      //const filepath = path.resolve(dir, filename);
+      const filename = 'frames-' + pad(index,4) +'.jpg';
+      const frame = fs.readFileSync(filename);
+
+      const image = frame.toString('base64');
+      socket.emit('image',image);
+      index++;
+
+      imageCount++;
+    }
+  }, 1000/FPS)
+
+  socket.on('PAYMENT_ACK',(data) =>{
+    console.log('*** Received PAYMENT_ACK ***');
+    console.log(`[Transcation hash: ${data}]`);
+    console.log('Resuming...')
+    setTimeout(function(){
+      isPaused = 0;
+    },1000);
+  })
+})
+
+
+
+//server.listen(3000);
